b9a24c337ea787700a8fe3c595392f704f69b423
Tobias Brunner <tobias.brunner@vshn.ch>
2016-11-21 10:43:49 +0100

    now chown possible in openshift

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -51,9 +51,6 @@ echo >&2 "======================================================================
 # Write the database connection to the config so the installer prefills it
 if ! [ -e app/config/local.php ]; then
         php /makeconfig.php "$MAUTIC_DB_HOST" "$MAUTIC_DB_USER" "$MAUTIC_DB_PASSWORD" "$MAUTIC_DB_NAME"
-
-        # Make sure our web user owns the config file if it exists
-        chown www-data:www-data app/config/local.php
 fi
 
 exec "$@"
